fa85d6fa9e51f342563066ca4a620311e8104050
Fred <fred@apigoat.com>
2020-12-02 10:58:21 +0800

    foreign column params

--- src/Column.php
+++ src/Column.php
@@ -84,7 +84,7 @@ class Column
         "auto-increment" => ["autoIncrement", "true"],
         "unique" => [],
         "index" => [],
-        "foreign" => []
+        "foreign(" => []
     ];
 
     /**
